a45b67fcec43a79f3cda572bb136446955941757
kushvash <kushvash.usc@gmail.com>
2025-03-15 00:47:03 -0700

    Prepare backend for cloud hosting

--- server/server.js
+++ server/server.js
@@ -28,4 +28,5 @@ app.post("/github/oauth", async (req, res) => {
 
 app.options("*", cors());
 
-app.listen(3000, '0.0.0.0', () => console.log("🚀 Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
